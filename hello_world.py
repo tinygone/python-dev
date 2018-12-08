3058a831d5871dc141896c1f7d9e3ca6b4f37bcd
tinygone <tinygone@qq.com>
2018-12-08 23:54:45 +0800

    dev

--- hello_world.py
+++ hello_world.py
@@ -23,3 +23,44 @@ print(bicycles[-1])
 
 print(bicycles[-1])
 
+# List operation
+magicians = ['alice', 'david', 'carolina']
+for mag in magicians:
+    print(mag)
+    print(mag.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + mag.title() + ".\n")
+print("Thank you, everyone. That was a great magic show!")
+
+for value in range(1, 5):
+    print(value)
+numbers = list(range(1, 6))
+print(numbers)
+
+# even numbers
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+print(squares)
+
+squares = [value ** 2 for value in range(1, 11)]
+print(squares)
+
+# slice
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+print(players[2:])
+print(players[:4])
+print(players[-3:])
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+
