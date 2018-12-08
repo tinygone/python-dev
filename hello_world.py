c3ee69c9c87bb90af83a9b6921e76fc56ab3978b
tinygone <tinygone@qq.com>
2018-12-08 23:36:27 +0800

    dev

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+favorite_language = ' python '
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
+
+msg = "Albert Einstein once said, \"A person who never made a mistake never tried anything new.\""
+print(msg)
+
+print(3 ** 2)
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+
+print(message)
+
+# import this
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[-1])
+
+print(bicycles[-1])
+
